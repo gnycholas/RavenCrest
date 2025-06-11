e4a823693a9040df51b477e22a1bf3deea2ca570
Gustavo Nycholas <gustavonycholas@gmail.com>
2025-06-10 22:48:09 -0300

    Update internal asset path

--- core/src/main/java/com/valleco/ravencrest/screens/GameScreen.java
+++ core/src/main/java/com/valleco/ravencrest/screens/GameScreen.java
@@ -29,7 +29,7 @@ public class GameScreen implements Screen {
         engine.addSystem(new AISystem());
 
         // Criar a entidade jogador
-        playerTexture = new Texture(Gdx.files.internal("assets/PlaceHolders/Sprites/idol1.png"));
+        playerTexture = new Texture(Gdx.files.internal("PlaceHolders/Sprites/idol1.png"));
         Entity player = EntityFactory.createPlayer(100, 100, playerTexture);
         engine.addEntity(player);
 
@@ -37,13 +37,13 @@ public class GameScreen implements Screen {
         engine.addSystem(new PickupSystem(player));
 
         // Criar itens coletáveis
-        Texture keyTexture = new Texture(Gdx.files.internal("assets/PlaceHolders/Sprites/key1.png"));
+        Texture keyTexture = new Texture(Gdx.files.internal("PlaceHolders/Sprites/key1.png"));
         engine.addEntity(EntityFactory.createItem(150, 150, keyTexture));
-        Texture coinTexture = new Texture(Gdx.files.internal("assets/PlaceHolders/Sprites/coin1.png"));
+        Texture coinTexture = new Texture(Gdx.files.internal("PlaceHolders/Sprites/coin1.png"));
         engine.addEntity(EntityFactory.createItem(300, 150, coinTexture));// Criar um item
 
         // Criar a entidade NPC que se move automaticamente
-        npcTexture = new Texture(Gdx.files.internal("assets/PlaceHolders/Sprites/Eidol1.png"));
+        npcTexture = new Texture(Gdx.files.internal("PlaceHolders/Sprites/Eidol1.png"));
         engine.addEntity(EntityFactory.createMovingNPC(200, 200, npcTexture, 30f, 0f));  // Movimento na direção X
     }
 
